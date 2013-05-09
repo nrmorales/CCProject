4fc87658c85bb0948f9a0ff2dccfd45aa4c9a54d
Noelyn Joyce Morales <noelyn34@gmail.com>
2013-05-09 16:57:20 +0800

    leh

--- apps/CC/android/native/assets/www/default/wlclient/js/checksum.js
+++ apps/CC/android/native/assets/www/default/wlclient/js/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":3923059981,"date":1368071051653,"machine":"AGC_PC187"};
-/* Date: Thu May 09 11:44:11 CST 2013 */
+var WL_CHECKSUM = {"checksum":3485898138,"date":1368089463016,"machine":"AGC_PC187"};
+/* Date: Thu May 09 16:51:03 CST 2013 */
